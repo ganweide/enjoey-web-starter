5a0e79cd4c361b85e1c49f4f9f7649bb6dd88ac9
ganweide1012 <ganweide@visualogic.com.my>
2023-10-03 14:21:19 +0800

    update

--- enjoey-web-starter/src/pages/sample/Learning/index.js
+++ enjoey-web-starter/src/pages/sample/Learning/index.js
@@ -1,5 +1,5 @@
 // React Imports
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
@@ -25,7 +25,6 @@ import {
   TableRow,
   TextField,
   FormControl,
-  IconButton,
   InputLabel,
   Select,
   MenuItem,
