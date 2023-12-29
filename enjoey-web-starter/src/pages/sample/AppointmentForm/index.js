6e7b46409cd29ff94707152269c50ca263c45be3
ganweide1012 <ganweide@visualogic.com.my>
2023-12-29 16:53:49 +0800

    commit

--- enjoey-web-starter/src/pages/sample/AppointmentForm/index.js
+++ enjoey-web-starter/src/pages/sample/AppointmentForm/index.js
@@ -40,6 +40,12 @@ import PropTypes from 'prop-types';
 // Local Imports
 import Styles from "./style";
 
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
 // Global Constants
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -75,6 +81,8 @@ function a11yProps(index) {
   };
 }
 
+const localizer = momentLocalizer(moment);
+const DnDCalendar = withDragAndDrop(Calendar);
 const useStyles = makeStyles(Styles);
 const appointmentUrl  = "http://localhost:8000/api/appointment/";
 const timeSlotUrl     = "http://localhost:8000/api/appointment-time-slots/";
@@ -386,6 +394,30 @@ const Page2 = () => {
     closeCreateTimeSlotsDialog();
   };
 
+  const event = {
+    events: [
+      {
+        start: moment().toDate(),
+        end: moment().add(1, "days").toDate(),
+        title: "Some title",
+      },
+    ],
+  };
+
+  const onEventResize = (data) => {
+    const { start, end } = data;
+
+    this.setState((state) => {
+      state.events[0].start = start;
+      state.events[0].end = end;
+      return { events: [...state.events] };
+    });
+  };
+
+  const onEventDrop = (data) => {
+    console.log(data);
+  };
+
   return (
     <Card sx={{ p: 5 }}>
       <Box sx={{ width: '100%' }}>
@@ -530,6 +562,16 @@ const Page2 = () => {
               ))}
             </TableBody>
           </Table>
+          <DnDCalendar
+            defaultDate={moment().toDate()}
+            defaultView="month"
+            events={event.events}
+            localizer={localizer}
+            onEventDrop={onEventDrop}
+            onEventResize={onEventResize}
+            resizable
+            style={{ height: "100vh" }}
+          />
         </TabPanel>
         <TabPanel value={tab} index={1}>
           <Box
